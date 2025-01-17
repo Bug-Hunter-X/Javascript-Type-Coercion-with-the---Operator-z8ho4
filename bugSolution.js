7c827ca084332dc638d16707d56dba75618c7774
BugHunter-X <askzenai@gmail.com>
2025-01-17 04:34:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,1 @@
+function foo(a,b){if(typeof a === 'number' && typeof b === 'number'){return a+b;}else{return "Invalid input: Operands must be numbers";}}console.log(foo(2,3));//5console.log(foo(2,"3"));//Invalid input: Operands must be numbersconsole.log(foo("2",3));//Invalid input: Operands must be numbersconsole.log(foo("2","3"));//Invalid input: Operands must be numbers
